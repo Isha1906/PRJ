8c11950c528f76b2f7689cff73b4108e4e879522
Isha1906 <111346925+Isha1906@users.noreply.github.com>
2022-08-18 16:25:24 +0530

    Update SportsCompetition.java

--- Sports_Competition/src/SportsCompetition.java
+++ Sports_Competition/src/SportsCompetition.java
@@ -11,21 +11,21 @@ public class SportsCompetition {                 //class_name-"SportsCompetition
 	roll_no=r.nextInt();
 	
 	// Checking students roll number using If Condition
-	if(roll_no==1 ||roll_no==5 ||roll_no==9 ||roll_no==13 ||roll_no==17 ||roll_no==21)    
+	if(roll_no==1 ||roll_no==5 ||roll_no==9 ||roll_no==13 ||roll_no==17 ||roll_no==21)    //Checking whether roll_number belongs to(1, 5, 9, 13, 17, 21)
 	{
-		System.out.print("--You Are in Sapphire Group--");  	//Assigning group According to there roll_number
+		System.out.print("--You Are in Sapphire Group--");  	r
 	}
-	else if(roll_no==2 ||roll_no==6 ||roll_no==10 ||roll_no==14 ||roll_no==18 ||roll_no==22)
+	else if(roll_no==2 ||roll_no==6 ||roll_no==10 ||roll_no==14 ||roll_no==18 ||roll_no==22)     //Checking whether roll_number belongs to(2, 6, 10, 14, 18, 22 )
 	{
-		System.out.print("--You Are in Perl Group--");          //Assigning group According to there roll_number
+		System.out.print("--You Are in Perl Group--");          
+		
+	else if(roll_no==3 ||roll_no==7 ||roll_no==11 ||roll_no==15 ||roll_no==19 ||roll_no==23)     //Checking whether roll_number belongs to(3, 7, 11, 15, 19, 23)
+	{
+		System.out.print("--You Are in Ruby Group--");         
 	}
-	else if(roll_no==3 ||roll_no==7 ||roll_no==11 ||roll_no==15 ||roll_no==19 ||roll_no==23)
+	else if(roll_no==4 ||roll_no==8 ||roll_no==12 ||roll_no==16 ||roll_no==20 ||roll_no==24)       //Checking whether roll_number belongs to(4, 8, 12, 16, 20, 24)  
 	{
-		System.out.print("--You Are in Ruby Group--");          //Assigning group According to there roll_number
-	}
-	else if(roll_no==4 ||roll_no==8 ||roll_no==12 ||roll_no==16 ||roll_no==20 ||roll_no==24)
-	{
-		System.out.print("--You Are in Perl Group--");          //Assigning group According to there roll_number
+		System.out.print("--You Are in Perl Group--");          
 	}
 	else System.out.println(" You Entered wrong Roll number!!!!");     //Giving Student/User warning that he/she entered wrong roll_number
 	}
